b50547cd3aae442898f125f2a824ce0956d02b07
Denis Matejcik <115463208+Mild-Solvent@users.noreply.github.com>
2024-01-23 00:23:11 +0100

    Update test_cases.cpp

--- LibrarySystem-main/tests/test_cases.cpp
+++ LibrarySystem-main/tests/test_cases.cpp
@@ -11,20 +11,7 @@ REQUIRE(patron.id == 1);
 }
 
 TEST_CASE("Book Adding", "[Book]") {
-// Assuming your `Book` class has a method to set details and save to file
+
 Book book;
-// You would set details here, such as `book.setDetails("Book Title", "Author", 123);`
-// Then check if the book details are correctly set
-// REQUIRE(book.getTitle() == "Book Title");
-// REQUIRE(book.getAuthor() == "Author");
-// REQUIRE(book.getPageCount() == 123);
 
-// If your `Book` class writes to a file, you can check the file contents as well
-// std::ifstream file("path_to_book_file.csv");
-// REQUIRE(file.good());
-// std::string contents;
-// std::getline(file, contents);
-// REQUIRE(!contents.empty());
 }
-
-// Add more test cases for different functionalities of your LibrarySystem
